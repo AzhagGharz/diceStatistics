21f34f14c4103b1af6d7115dab9a73fad2960a6c
MateuszGalasinski <mg@DESKTOP-G7VF8SG.localdomain>
2022-12-22 12:32:17 +0100

    slow async

--- src/ConsoleApp/Program.fs
+++ src/ConsoleApp/Program.fs
@@ -53,67 +53,64 @@ type SimplifiedRollResult =
 let listInitResult length result = List.init length (fun _ -> result)
 
 let possibleDiceResults dice =
-  match dice with
-  | Dice.Boost ->
-    (listInitResult 2 emptyResult)
-    @ [ { emptyResult with Successes = 1 }
-        { emptyResult with Advantages = 1 }
-        { emptyResult with
-            Advantages = 1
-            Successes = 1 }
-        { emptyResult with Advantages = 2 } ]
-  | Dice.Ability ->
-    (listInitResult 2 emptyResult)
-    @ (listInitResult 2 { emptyResult with Advantages = 1 })
-      @ (listInitResult 2 { emptyResult with Successes = 1 })
-        @ [ { emptyResult with
+  async {
+    return
+      match dice with
+      | Dice.Boost ->
+        (listInitResult 2 emptyResult)
+        @ [ { emptyResult with Successes = 1 }
+            { emptyResult with Advantages = 1 }
+            { emptyResult with
                 Advantages = 1
                 Successes = 1 }
-            { emptyResult with Successes = 2 }
             { emptyResult with Advantages = 2 } ]
-  | Dice.Proficiency ->
-    (listInitResult 1 emptyResult)
-    @ (listInitResult 2 { emptyResult with Successes = 1 })
-      @ (listInitResult 2 { emptyResult with Successes = 2 })
-        @ (listInitResult 2 { emptyResult with Advantages = 2 })
-          @ (listInitResult
-               3
-               { emptyResult with
-                   Advantages = 1
-                   Successes = 1 })
-            @ [ { emptyResult with Advantages = 1 }; { emptyResult with Triumphs = 1 } ]
-  | Dice.Setback ->
-    (listInitResult 2 emptyResult)
-    @ (listInitResult 2 { emptyResult with Disadvantages = 1 })
-      @ (listInitResult 2 { emptyResult with Failures = 1 })
-  | Dice.Difficulty ->
-    (listInitResult 1 emptyResult)
-    @ (listInitResult 2 { emptyResult with Disadvantages = 2 })
-      @ (listInitResult 2 { emptyResult with Failures = 2 })
-        @ (listInitResult 3 { emptyResult with Disadvantages = 1 })
-          @ [ { emptyResult with
-                  Disadvantages = 1
-                  Successes = 1 }
-              { emptyResult with Advantages = 2 } ]
-  | Dice.Challenge ->
-    (listInitResult 1 emptyResult)
-    @ (listInitResult 2 { emptyResult with Disadvantages = 1 })
-      @ (listInitResult 2 { emptyResult with Disadvantages = 2 })
-        @ (listInitResult 2 { emptyResult with Failures = 1 })
+      | Dice.Ability ->
+        (listInitResult 1 emptyResult)
+        @ (listInitResult 2 { emptyResult with Advantages = 1 })
+          @ (listInitResult 2 { emptyResult with Successes = 1 })
+            @ [ { emptyResult with
+                    Advantages = 1
+                    Successes = 1 }
+                { emptyResult with Successes = 2 }
+                { emptyResult with Advantages = 2 } ]
+      | Dice.Proficiency ->
+        (listInitResult 1 emptyResult)
+        @ (listInitResult 2 { emptyResult with Successes = 1 })
+          @ (listInitResult 2 { emptyResult with Successes = 2 })
+            @ (listInitResult 2 { emptyResult with Advantages = 2 })
+              @ (listInitResult
+                   3
+                   { emptyResult with
+                       Advantages = 1
+                       Successes = 1 })
+                @ [ { emptyResult with Advantages = 1 }; { emptyResult with Triumphs = 1 } ]
+      | Dice.Setback ->
+        (listInitResult 2 emptyResult)
+        @ (listInitResult 2 { emptyResult with Disadvantages = 1 })
+          @ (listInitResult 2 { emptyResult with Failures = 1 })
+      | Dice.Difficulty ->
+        (listInitResult 1 emptyResult)
+        @ (listInitResult 2 { emptyResult with Disadvantages = 2 })
           @ (listInitResult 2 { emptyResult with Failures = 2 })
-            @ (listInitResult
-                 2
-                 { emptyResult with
-                     Disadvantages = 1
-                     Failures = 1 })
-              @ [ { emptyResult with Despairs = 1 } ]
-  // TODO: remove test implementation
-  | Dice.TestDice1 ->
-    [ emptyResult
-      { emptyResult with Advantages = 100 }
-      { emptyResult with Successes = 100 } ]
-  | Dice.TestDice2 -> [ emptyResult; { emptyResult with Advantages = 10 } ]
-  | _ -> failwithf "%s is not a known dice type" (dice.ToString())
+            @ (listInitResult 3 { emptyResult with Disadvantages = 1 })
+              @ [ { emptyResult with
+                      Disadvantages = 1
+                      Successes = 1 }
+                  { emptyResult with Advantages = 2 } ]
+      | Dice.Challenge ->
+        (listInitResult 1 emptyResult)
+        @ (listInitResult 2 { emptyResult with Disadvantages = 1 })
+          @ (listInitResult 2 { emptyResult with Disadvantages = 2 })
+            @ (listInitResult 2 { emptyResult with Failures = 1 })
+              @ (listInitResult 2 { emptyResult with Failures = 2 })
+                @ (listInitResult
+                     2
+                     { emptyResult with
+                         Disadvantages = 1
+                         Failures = 1 })
+                  @ [ { emptyResult with Despairs = 1 } ]
+      | _ -> failwithf "%s is not a known dice type" (dice.ToString())
+  }
 
 let addDiceResults (results: RollResult list) = List.reduce (+) results
 // { Advantages = List.sumBy (fun r -> r.Advantages) results
@@ -123,28 +120,46 @@ let addDiceResults (results: RollResult list) = List.reduce (+) results
 //   Failures = List.sumBy (fun r -> r.Failures) results
 //   Despairs = List.sumBy (fun r -> r.Despairs) results }
 
-let traverseDiceSet (dices: Dice list) =
-  let multiplyPossibleResults (fst: RollResult seq) (snd: RollResult seq) =
-    seq {
-      for fstRes in fst do
-        yield! seq { for sndRes in snd -> fstRes + sndRes }
-    }
+let traverseDiceSet diceResults =
+  let multiplyPossibleResults (fst: RollResult list) (snd: RollResult list) : RollResult list =
+    let partialResults =
+      fst
+      |> List.map (fun fstRes -> async { return List.map (fun sndRes -> fstRes + sndRes) snd })
+      |> fun res -> Async.Parallel(res, 8)
+      |> Async.RunSynchronously
+
+    List.fold
+      (fun acc elemArray -> List.fold (fun acc2 elem -> elem :: acc2) acc elemArray)
+      []
+      (Array.toList partialResults)
+  // seq {a
+  //   for fstRes in fst do
+  //     yield! seq { for sndRes in snd -> fstRes + sndRes }
+  // }
 
   let rec generate dices acc =
     match dices with
-    | dice :: tail -> generate tail (multiplyPossibleResults acc (possibleDiceResults dice))
+    | dice :: tail -> generate tail (multiplyPossibleResults acc dice)
     | [] -> acc
-  // let generate dices =
-  //   seq {
-  //     yield possibleDiceResults dices.Item
-  //   }
-  generate dices [ emptyResult ]
 
-let testExample = traverseDiceSet [ Dice.TestDice1; Dice.TestDice2; Dice.TestDice2 ]
-printfn "Got %d results" (Seq.length testExample)
+  generate diceResults [ emptyResult ]
 
-for result in testExample do
-  printfn "Result:"
-  printfn "%s%s" (result.prettyPrint ()) Environment.NewLine
+let timeMeasuredFunction f =
+  let timer = System.Diagnostics.Stopwatch.StartNew()
+  f ()
+  timer.Stop()
+  printfn "Took %fms" timer.Elapsed.TotalMilliseconds
 
-printfn "End"
+let runTest dicePool =
+  let testExample =
+    dicePool
+    |> List.map possibleDiceResults
+    |> fun res -> Async.Parallel(res, 8)
+    |> Async.RunSynchronously
+    |> Array.toList
+    |> traverseDiceSet
+
+  printfn "Got %d results" (Seq.length testExample)
+
+for i in seq { 8..9 } do
+  timeMeasuredFunction (fun _ -> runTest (List.init i (fun _ -> Dice.Ability)))
